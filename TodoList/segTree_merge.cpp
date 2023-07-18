40f1a2ef70c32cdf33790347ccb5acee318805c8
caotianlang <caotianlang@outlook.com>
2023-07-18 12:49:58 +0800

    segTree_merge

---
+++ TodoList/segTree_merge.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+pair<int,int>
+
+pii max(pii x,y){
+	if(x.f!=y.f)return x.f>y.f?x:y;
+	else return x.
+}
+
+pushup(){
+	now=max(lll,rrr);
+}
+
+
+
+void merge(int &x,int y,int l,int r){
+	if(!x||!y)x|=y;
+	else if(l==r){
+		t[x].val+=t[y].val;
+	} 
+	else{
+		int mid;
+		merge(llll);
+		merge(rrrr);
+		pushup(x);
+	}
+}
+
+int main ()
+{
+
+	return 0;
+}
+
