a8683c1e1b9344fdd5b4c12be2b37413ec7a9d45
caotianlang <caotianlang@outlook.com>
2023-08-14 01:02:52 +0800

    P4136

---
+++ Luogu/P4136.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <queue>
+
+#define fastread ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define endl "\n"
+
+using namespace std;
+
+
+int main () {
+	fastread
+	
+	while (true) {
+		int n; cin >> n;
+		if (not n) { return 0; }
+		cout << ((n % 2) ? "Bob" : "Alice") << endl;
+	}
+
+	return 0;
+}
