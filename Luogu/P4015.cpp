083f392fe5b3f9c6b69c4048de4273d2cd898972
caotianlang <caotianlang@outlook.com>
2023-08-21 21:52:37 +0800

    P4015

---
+++ Luogu/P4015.cpp
@@ -0,0 +1,172 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <queue>
+
+#define fastread ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define endl "\n"
+
+using namespace std;
+
+const int maxn = 205;
+const int maxm = 20405;
+
+namespace Net {
+	struct Edge {
+		int u, v;
+		int pre;
+		int flow;
+		int cost;
+	} es[maxm];
+
+	int last[maxn], cur[maxn], cnt;
+	int S, T;
+	int cost;
+
+	inline void _addEdge (int u, int v, int cap, int cost) {
+		es[++cnt] = Edge { u, v, last[u], cap, cost };
+		last[u] = cnt;
+	}
+
+	inline void addEdge (int u, int v, int cap, int cost) {
+		_addEdge (u, v, cap, cost);
+		_addEdge (v, u, 0, -cost);
+	}
+
+	inline void clear () {
+		memset (last, 0, sizeof(last));
+		cnt = 1; cost = 0;
+	}
+
+	int dis[maxn];
+	bool vis[maxn];
+
+	inline bool spfa () {
+		memset (dis, 0x3f, sizeof(dis));
+		memset (vis, 0, sizeof(vis));
+		queue<int> q; q.push (S); dis[S] = 0; vis[S] = true;
+		
+		while (not q.empty()) {
+			int now = q.front(); q.pop();
+			vis[now] = false;
+
+			for (int i = last[now]; i; i = es[i].pre) {
+				int t = es[i].v;
+				if (not es[i].flow) { continue; }
+				if (dis[t] > dis[now] + es[i].cost) {
+					dis[t] = dis[now] + es[i].cost;
+					if (not vis[t]) { q.push(t); vis[t] = true; }
+				}
+			}
+		}
+
+		return dis[T] != 0x3f3f3f3f;
+	}
+
+	int dfs (int now, int now_flow) {
+		if (now == T or not now_flow) { return now_flow; }
+		
+		vis[now] = true;
+		int res = 0;
+		for (int &i = cur[now]; i; i = es[i].pre) {
+			int t = es[i].v;
+
+			if (not es[i].flow) { continue; }
+			if (vis[t]) { continue; }
+			if (dis[t] != dis[now] + es[i].cost) { continue; }
+
+			int t_flow = dfs (t, min (es[i].flow, now_flow));
+			if (t_flow) {
+				es[i].flow -= t_flow;
+				es[i ^ 1].flow += t_flow;
+				now_flow -= t_flow;
+				res += t_flow;
+
+				cost += t_flow * es[i].cost;
+
+				if (not now_flow) { break; }
+			}
+		}
+
+		vis[now] = false;
+		return res;
+	}
+
+	int dinic (int _s, int _t) {
+		S = _s, T = _t;
+
+		int max_flow = 0;
+		while (spfa ()) {
+			memcpy (cur, last, sizeof (last));
+			max_flow += dfs (S, 0x3f3f3f3f);
+		}
+
+		return cost;
+	}
+
+}
+
+int m, n;
+
+int a[maxn], b[maxn];
+int c[maxn][maxn];
+
+int tot;
+inline int newNode () { return ++tot; }
+
+int a_id[maxn], b_id[maxn];
+int S, T;
+
+inline void buildGraph (int flag) {
+	tot = 0;
+
+	for (int i = 1; i <= m; i++) { a_id[i] = newNode (); }
+	for (int i = 1; i <= n; i++) { b_id[i] = newNode (); }
+	S = newNode (), T = newNode ();	
+
+	for (int i = 1; i <= m; i++) { Net::addEdge (S, a_id[i], a[i], 0); }
+	for (int i = 1; i <= n; i++) { Net::addEdge (b_id[i], T, b[i], 0); }
+
+	for (int i = 1; i <= m; i++) {
+		for (int j = 1; j <= n; j++) {
+			Net::addEdge (a_id[i], b_id[j], 0x3f3f3f3f, c[i][j] * flag);
+		}
+	}
+}
+
+int main () {
+	// fastread
+
+	cin >> m >> n;
+
+	for (int i = 1; i <= m; i++) { cin >> a[i]; }
+	
+	for (int i = 1; i <= n; i++) { cin >> b[i]; }
+
+	for (int i = 1; i <= m; i++) {
+		for (int j = 1; j <= n; j++) {
+			cin >> c[i][j];
+		}
+	}
+
+	Net::clear ();
+	buildGraph (1);
+	int min_cost = Net::dinic (S, T);
+
+	Net::clear ();
+	buildGraph (-1);
+	int max_cost = -Net::dinic (S, T);
+
+	cout << min_cost << endl;
+	cout << max_cost << endl;
+
+	return 0;
+}
