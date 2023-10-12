c782af368571e1d18e1fc08afc6c0af37eaac33a
caotianlang <caotianlang@outlook.com>
2023-10-12 08:59:35 +0800

    update model.cpp

--- model.cpp
+++ model.cpp
@@ -11,12 +11,11 @@
 #include <cmath>
 #include <queue>
 
-#define fastread ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-#define endl "\n"
-
 using std::cin;
 using std::cout;
 
+#define fastread std::ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define endl "\n"
 
 int main () {
 	fastread
