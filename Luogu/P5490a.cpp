cc8ece49523c88b7859699dfa05d2c04355e844b
caotianlang <caotianlang@outlook.com>
2024-01-27 15:02:23 +0000

    P5490a

---
+++ Luogu/P5490a.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <queue>
+
+using i64 = long long;
+
+#define fastread std::ios::sync_with_stdio(false); std::cin.tie(0);
+
+const int maxn = 100005;
+
+struct Line {
+	int x;
+	int y1, y2;
+
+	int mark;
+	
+	friend bool operator < (Line a, Line b) {
+		return a.x < b.x;
+	}
+} lines[maxn << 1];
+int tot;
+
+int n;
+
+struct Node {
+	int len;
+	int mark;
+
+	int ls, rs;
+} tr[maxn << 6];
+
+int nodeTot, root;
+
+inline void pushUp (int now, int l, int r) {
+	if (tr[now].mark) { tr[now].len = r - l + 1; }
+	else { tr[now].len = tr[tr[now].ls].len + tr[tr[now].rs].len; }
+}
+
+void modify (int &now, int l, int r, int L, int R, int val) {
+	if (not now) { now = ++nodeTot; } 
+	if (L <= l and r <= R) { tr[now].mark += val; pushUp (now, l, r); return; }	
+
+	int mid = (l + r) >> 1;
+	if (L <= mid) { modify (tr[now].ls, l, mid, L, R, val); }
+	if (R > mid) { modify (tr[now].rs, mid + 1, r, L, R, val); }
+
+	pushUp (now, l, r);	
+}
+
+inline int query () { return tr[root].len; }
+
+int main () {
+	fastread
+
+	std::cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		int x1, y1, x2, y2; std::cin >> x1 >> y1 >> x2 >> y2;
+		lines[++tot] = Line { x1, y1, y2, 1 };
+		lines[++tot] = Line { x2, y1, y2, -1 };
+	}
+
+	std::sort (lines + 1, lines + tot + 1);
+
+	i64 res = 0;
+	int last = 0;
+
+	for (int i = 1; i <= tot; i++) {
+		int x = lines[i].x;
+		res += 1ll * (x - last) * query ();
+		last = x;
+
+		modify (root, 0, 1e9, lines[i].y1, lines[i].y2 - 1, lines[i].mark);
+	}
+
+	std::cout << res << "\n";
+
+	return 0;
+}
