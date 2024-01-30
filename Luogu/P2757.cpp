3b176894b06233e88f4344e385fdc8e785e580a3
caotianlang <caotianlang@outlook.com>
2024-01-30 15:28:17 +0800

    P2757

---
+++ Luogu/P2757.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <queue>
+
+using i64 = long long;
+
+#define fastread std::ios::sync_with_stdio(false); std::cin.tie(0);
+
+const int maxn = 500005;
+const i64 mod = 1e9 + 7;
+const i64 base = 3;
+
+i64 p[maxn];
+
+int T;
+
+int n;
+int a[maxn];
+
+struct Node {
+	i64 hash1, hash2;
+	int len;
+} tr[maxn << 3];
+
+inline Node merge (Node a, Node b) {
+	Node res;
+	res.len = a.len + b.len;
+	res.hash1 = (a.hash1 * p[b.len] % mod + b.hash1) % mod;
+	res.hash2 = (b.hash2 * p[a.len] % mod + a.hash2) % mod;
+	return res;
+}	
+
+void build (int now, int l, int r) {
+	if (l == r) { tr[now] = Node { 0, 0, 1 }; return; }
+
+	int mid = (l + r) >> 1;
+	build (now << 1, l, mid);
+	build (now << 1 | 1, mid + 1, r);
+
+	tr[now] = merge (tr[now << 1], tr[now << 1 | 1]);
+}
+
+void modify (int now, int l, int r, int pos) {
+	if (l == r) { tr[now].hash1 = tr[now].hash2 = 1; return; }
+
+	int mid = (l + r) >> 1;
+
+	if (pos <= mid) { modify (now << 1, l, mid, pos); }
+	else { modify (now << 1 | 1, mid + 1, r, pos); }
+
+	tr[now] = merge (tr[now << 1], tr[now << 1 | 1]);
+}
+
+Node query (int now, int l, int r, int L, int R) {
+	if (L <= l and r <= R) { return tr[now]; }
+
+	int mid = (l + r) >> 1;
+
+	if (R <= mid) { return query (now << 1, l, mid, L, R); }
+	if (L > mid) { return query (now << 1 | 1, mid + 1, r, L, R); }
+
+	return merge (query (now << 1, l, mid, L, R), query (now << 1 | 1, mid + 1, r, L, R));
+}
+
+int main () {
+	fastread
+
+	p[0] = 1ll;
+	for (int i = 1; i < maxn; i++) { p[i] = p[i - 1] * base % mod; }
+
+	std::cin >> T;
+
+	while (T--) {
+		std::cin >> n;
+
+		for (int i = 1; i <= n; i++) { std::cin >> a[i]; }
+
+		build (1, 1, n);
+
+		for (int i = 1; i <= n; i++) {
+			modify (1, 1, n, a[i]);
+
+			int d = std::min (a[i] - 1, n - a[i]);
+
+			if (query (1, 1, n, a[i] - d, a[i]).hash1 != query (1, 1, n, a[i], a[i] + d).hash2 and i >= 2) {
+				std::cout << "Y" << "\n"; goto Yes;
+			}
+		}
+
+		std::cout << "N" << "\n";
+
+		Yes: ;
+	}
+
+	return 0;
+}
