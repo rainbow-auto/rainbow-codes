89d455f3c5bd2ac6f925fc50d332706a286231e0
caotianlang <caotianlang@outlook.com>
2024-02-09 09:38:30 +0800

    P6136

--- Luogu/P6136.cpp
+++ Luogu/P6136.cpp
@@ -11,38 +11,39 @@
 #include <cmath>
 #include <queue>
 
-#include <random>
 #include <chrono>
+#include <random>
 
 using i64 = long long;
 
 #define fastread std::ios::sync_with_stdio(false); std::cin.tie(0);
 
-const int maxn = 2000005;
+const int maxn = 1100005;
+
+int n, m;
 
 struct Node {
 	int ls, rs;
 	int val;
 	int siz;
+
 	unsigned long long key;
 } tr[maxn];
 
 int tot, root;
 
-std::mt19937 rnd (std::chrono::steady_clock::now().time_since_epoch().count());
+inline void pushUp (int now) { tr[now].siz = tr[tr[now].ls].siz + tr[tr[now].rs].siz + 1; }
+
+std::mt19937 rnd (std::chrono::steady_clock::now ().time_since_epoch ().count ());
 
 inline int newNode (int val) {
 	tr[++tot] = Node { 0, 0, val, 1, rnd () };
 	return tot;
 }
 
-inline void pushUp (int now) {
-	tr[now].siz = tr[tr[now].ls].siz + tr[tr[now].rs].siz + 1;
-}
-
 void split (int now, int val, int& x, int& y) {
-	if (not now) { x = 0; y = 0; }
-
+	if (not now) { x = y = 0; return; }
+	
 	if (tr[now].val <= val) {
 		x = now; split (tr[now].rs, val, tr[now].rs, y); pushUp (now);
 	} else {
@@ -68,10 +69,10 @@ inline void insert (int val) {
 }
 
 inline void remove (int val) {
-	int x, y, z;
-	split (root, val, x, y);
-	split (x, val - 1, x, z);
-	z = merge (tr[z].ls, tr[z].rs);	
+	int x, y, z; 
+	split (root, val, x, y); 
+		split (x, val - 1, x, z);
+	z = merge (tr[z].ls, tr[z].rs);
 	root = merge (merge (x, z), y);
 }
 
@@ -84,67 +85,49 @@ inline int getRank (int val) {
 
 inline int getNum (int rank) {
 	int now = root;
-
 	while (now) {
 		if (tr[tr[now].ls].siz + 1 == rank) { return tr[now].val; }
-		else if (tr[tr[now].ls].siz >= rank) { now = tr[now].ls; }
-		else { rank -= tr[tr[now].ls].siz + 1; now = tr[now].rs; }
+		else if (rank <= tr[tr[now].ls].siz) { now = tr[now].ls; }
+		else { rank -= tr[tr[now].ls].siz + 1; now = tr[now].rs;  }
 	}
-
-	return -1;
 }
 
 inline int pre (int val) {
-	int x, y;
-	split (root, val - 1, x, y);
-	
+	int x, y; split (root, val - 1, x, y);
 	int now = x;
 	while (tr[now].rs) { now = tr[now].rs; }
-
 	root = merge (x, y);
-
 	return tr[now].val;
-}
+}	
 
 inline int nxt (int val) {
 	int x, y; split (root, val, x, y);
-
 	int now = y;
 	while (tr[now].ls) { now = tr[now].ls; }
-
 	root = merge (x, y);
-
 	return tr[now].val;
 }
 
 int main () {
 	fastread
 
-    int n, Q; std::cin >> n >> Q;
-    
-    for (int i = 1; i <= n; i++) { int x; std::cin >> x; insert (x); } 
+	std::cin >> n >> m;
 
-    int lastans = 0;
-    int ans = 0;
+	for (int i = 1; i <= n; i++) { int x; std::cin >> x; insert (x); }
 
-	while (Q--) {
+	int ans = 0;
+
+	int lastans = 0;
+	while (m--) {
 		int op, x; std::cin >> op >> x;
+		x ^= lastans;
 
-        x ^= lastans;
-
-		if (op == 1) {
-			insert (x);
-		} else if (op == 2) {
-			remove (x);
-		} else if (op == 3) {
-			lastans = getRank (x); ans ^= lastans;
-		} else if (op == 4) {
-			lastans = getNum (x); ans ^= lastans;
-		} else if (op == 5) {
-			lastans = pre (x); ans ^= lastans;
-		} else if (op == 6) {
-			lastans = nxt (x); ans ^= lastans;
-		}
+		if (op == 1) { insert (x); }		
+		if (op == 2) { remove (x); }
+		if (op == 3) { lastans = getRank (x); ans ^= lastans; }
+		if (op == 4) { lastans = getNum (x); ans ^= lastans; }
+		if (op == 5) { lastans = pre (x); ans ^= lastans; }
+		if (op == 6) { lastans = nxt (x); ans ^= lastans; }
 	}
 
 	std::cout << ans << "\n";
