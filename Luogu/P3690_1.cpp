fc96be13f510717b8601196f918520f4c58ce963
caotianlang <caotianlang@outlook.com>
2024-02-20 10:27:03 +0800

    P3690_1

--- Luogu/P3690_1.cpp
+++ Luogu/P3690_1.cpp
@@ -15,52 +15,53 @@ using i64 = long long;
 
 #define fastread std::ios::sync_with_stdio(false); std::cin.tie(0);
 
-const int maxn = 300005;
+const int maxn = 100005;
 
-struct Splay {
+int n, m;
+int a[maxn];
+
+struct Node {
+	int sum, val;
 	int ch[2];
 	int fa;
-	
-	int val;
-	int sum;
-	
-	int tag;
+	bool tag;
 } tr[maxn];
 
-inline bool chk (int x) { return x == tr[tr[x].fa].ch[1]; }
-inline bool notRoot (int x) { return x == tr[tr[x].fa].ch[0] or x == tr[tr[x].fa].ch[1]; }
-inline void pushUp (int x) { tr[x].sum = tr[tr[x].ch[0]].sum xor tr[tr[x].ch[1]].sum xor tr[x].val; }
+inline bool chk (int now) { return now == tr[tr[now].fa].ch[1]; }
+inline bool notRoot (int now) { return now == tr[tr[now].fa].ch[0] or now == tr[tr[now].fa].ch[1]; }
 
-inline void rev (int x) { std::swap (tr[x].ch[0], tr[x].ch[1]); tr[x].tag ^= 1; }
+inline void pushUp (int now) { tr[now].sum = tr[tr[now].ch[0]].sum xor tr[tr[now].ch[1]].sum xor tr[now].val; }
 
-inline void pushDown (int x) {
-	if (not tr[x].tag) { return; }
-	if (tr[x].ch[0]) { rev (tr[x].ch[0]); }
-	if (tr[x].ch[1]) { rev (tr[x].ch[1]); }
-	tr[x].tag = false;
+inline void rev (int now) { std::swap (tr[now].ch[0], tr[now].ch[1]); tr[now].tag ^= 1; }
+inline void pushDown (int now) {
+	if (not tr[now].tag) { return; }
+	if (tr[now].ch[0]) { rev (tr[now].ch[0]); }
+	if (tr[now].ch[1]) { rev (tr[now].ch[1]); }
+	tr[now].tag = false;
 }
 
-inline void connect (int f, int s, int p) { tr[f].ch[p] = s; tr[s].fa = f; }
+void pushAll (int now) {
+	if (notRoot (now)) { pushAll (tr[now].fa); }
+	pushDown (now);
+}
+
+inline void connect (int fa, int x, int p) { tr[fa].ch[p] = x; tr[x].fa = fa; }
 
 inline void rotate (int x) {
-	int f = tr[x].fa, ff = tr[f].fa, p = chk (x), pp = chk (f);
+	int f = tr[x].fa, ff = tr[f].fa, p = chk (x), pp = chk (f); 
 	pushDown (f); pushDown (x);
-	if (notRoot (f)) { connect (ff, x, pp); } else { tr[x].fa = ff; } // 只认父亲
+	if (notRoot (f)) { connect (ff, x, pp); }
+	else { tr[x].fa = ff; }
 	connect (f, tr[x].ch[p ^ 1], p); connect (x, f, p ^ 1);
 	pushUp (f); pushUp (x);
 }
 
-inline void pushAll (int x) {
-	if (notRoot (x)) { pushAll (tr[x].fa); }
-	pushDown (x);
-}
-
 inline void splay (int x) {
 	pushAll (x);
-	for (; notRoot (x); rotate (x))	{ 
+	for (; notRoot (x); rotate (x)) {
 		if (notRoot (tr[x].fa)) { rotate (chk (tr[x].fa) == chk (x) ? tr[x].fa : x); }
 	}
-	pushUp (x);
+	pushUp (x);	
 }
 
 inline void access (int x) {
@@ -73,28 +74,29 @@ inline void access (int x) {
 
 inline void makeRoot (int x) { access (x); splay (x); rev (x); }
 inline void split (int x, int y) { makeRoot (x); access (y); splay (y); }
+
 inline int findRoot (int x) {
 	access (x); splay (x); pushDown (x);
 	while (tr[x].ch[0]) { x = tr[x].ch[0]; pushDown (x); }
 	splay (x); return x;
 }
+
 inline void link (int x, int y) { 
 	makeRoot (x); 
 	if (x == findRoot (y)) { return; } 
 	tr[x].fa = y;
 }
+
 inline void cut (int x, int y) { 
 	split (x, y); 
-	if (tr[y].ch[0] != x or tr[x].ch[0] != tr[x].ch[1]) { return; } 
-	tr[x].fa = tr[y].ch[0] = 0; 
+	if (tr[y].ch[0] != x or tr[x].ch[0] != tr[x].ch[1]) { return; }
+	tr[y].ch[0] = tr[x].fa = 0; 
 	pushUp (y);
 }
 
-int n, m;
-
 int main () {
 	fastread
-
+	
 	std::cin >> n >> m;
 
 	for (int i = 1; i <= n; i++) { std::cin >> tr[i].val; }
@@ -105,7 +107,7 @@ int main () {
 		if (op == 1) { link (x, y); }
 		if (op == 2) { cut (x, y); }
 		if (op == 3) { splay (x); tr[x].val = y; pushUp (x); }
-	}
+	}	
 
 	return 0;
 }
