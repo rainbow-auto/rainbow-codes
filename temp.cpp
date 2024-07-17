e1053c8dcc373b3840be09f89c01d83d4d5fc7b4
caotianlang <caotianlang@outlook.com>
2024-07-17 08:00:28 +0800

    P10766

--- temp.cpp
+++ temp.cpp
@@ -15,52 +15,8 @@ using i64 = long long;
 
 #define fastread std::ios::sync_with_stdio(false); std::cin.tie(0);
 
-const int maxn = 500005;
-
-std::string s, t;
-
-int dp[maxn][3];
-inline void solve () {
-	memset (dp, 0x3f, sizeof (dp));
-	
-	dp[1][0] = 1 + (t[i] != '1');
-	dp[1][1] = 1 + (t[i] != '0');
-	dp[1][2] = (s[i] != t[i]);
-
-	for (int i = 2; i <= n; i++) {
-		for (int now = 1; now <= 3; now ++) {
-			for (int pre = 1; pre <= 3; pre ++) {
-				int cost1 = 0;
-				if (now == 2) { cost1 = 0; }
-				else { cost1 = (now != pre); }
-				
-				int now_c = (now == 2) ? (s[i] - '0')     : now;
-				int pre_c = (pre == 2) ? (s[i - 1] - '0') : pre;
-				int cost2 = ((now_c ^ (t[i] - '0')) == 1 and (pre_c ^ (t[i - 1] - '0')) == 0);
-
-				dp[i][now] = std::min (dp[i][now], dp[i - 1][pre] + cost1 + cost2);
-			}
-		}
-	}
-
-	int res = 0x3f3f3f3f;
-	res = std::min (res, dp[n][0]);
-	res = std::min (res, dp[n][1]);
-	res = std::min (res, dp[n][2]);
-	std::cout << res << "\n";
-}
-
 int main () {
 	fastread
-
-	int T; std::cin >> T;
-
-	while (T--) {
-		std::cin >> s >> t;
-
-		solve ();
-	}
 	
-
 	return 0;
 }
