79b935123b67734da372b8bcdb3904f4ce7c5512
caotianlang <caotianlang@outlook.com>
2024-08-13 20:31:27 +0800

    CF1646E

---
+++ Luogu/CF1646E.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <queue>
+
+using i64 = long long;
+
+#define RainbowAutomatonDebugFlag
+
+#define fastread std::ios::sync_with_stdio(false); std::cin.tie(0);
+
+#define rep(QWQ, qwq, qaq) for (int QWQ = (qwq); (QWQ) <= (qaq); QWQ++)
+#define per(QWQ, qwq, qaq) for (int QWQ = (qwq); (QWQ) >= (qaq); QWQ--)
+
+#ifdef RainbowAutomatonDebugFlag
+#define db(x) std::cerr << #x << " : " << x << "\n";
+#else
+#define db(x) void (0);
+#endif
+
+int main () {
+	fastread
+	
+	return 0;
+}
