c3b13c86597cc1ca1e1d99f4d86caecd4d65892b
rainbow-auto <caotianlang@outlook.com>
2024-10-15 09:39:14 +0800

    P3435

---
+++ Luogu/P3435.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+// #pragma GCC optimize(2)
+
+using i64 = long long;
+using f64 = long double;
+
+#define fastread std::ios::sync_with_stdio (false); std::cin.tie(nullptr);
+
+#define rep(QWQ, qwq, qaq) for (i64 QWQ = (qwq); (QWQ) <= (qaq); QWQ++)
+#define per(QWQ, qwq, qaq) for (i64 QWQ = (qwq); (QWQ) >= (qaq); QWQ--)
+
+#define dbg(x) std::cerr << (#x) << " : " << x << "\n";
+#define dbendl std::cerr << "\n"; 
+#define db std::cerr
+
+#define lookMem std::cerr << abs (&MemST - &MemED) / 1024.0 / 1024.0 << "MB defined\n";
+#define lookTime std::cerr << (double) (clock () - TimeST) / CLOCKS_PER_SEC << "s used\n";
+int TimeST;
+bool MemST;
+// #define MultiTask lovely_fairytale
+
+std::vector<int> get_pi(std::string s) {
+	std::vector<int> pi(s.length());
+	rep (i, 2, s.length() - 1) {
+		int j = pi[i - 1];
+		while (j and s[j + 1] != s[i]) j = pi[j];
+		if (s[j + 1] == s[i]) j++;
+		pi[i] = j;
+	}
+	return pi;
+}
+
+void solve() {
+	int n; std::cin >> n;
+
+	std::string s; std::cin >> s; s = " " + s;
+
+	auto pi = get_pi(s);
+
+	std::vector<int> mnbd(pi.size());
+
+	i64 ans = 0;
+
+	rep (i, 2, n) {
+		int j = i;
+		while (pi[j]) j = pi[j];
+		if (pi[i]) pi[i] = j;
+		ans += i - j;
+	}
+
+	std::cout << ans << "\n";
+}
+
+bool MemED;
+int main() {
+	fastread
+	// lookMem	
+	// TimeST = clock ();
+
+#ifndef MultiTask
+	int _ = 1;
+#else
+	int _; std::cin >> _;
+#endif
+	
+	while (_--) {
+		solve();
+	}
+
+	return 0;
+}
