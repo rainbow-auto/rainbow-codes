47d295dda16d53a2ea00885ff85a37661c5bed27
rainbow-auto <caotianlang@outlook.com>
2024-10-19 10:49:30 +0800

    UVA11573

---
+++ Luogu/UVA11573.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+// #pragma GCC optimize(2)
+
+using i64 = long long;
+using f64 = long double;
+
+#define fastread std::ios::sync_with_stdio (false); std::cin.tie(nullptr);
+
+#define rep(QWQ, qwq, qaq) for (i64 QWQ = (qwq); (QWQ) <= (qaq); QWQ++)
+#define per(QWQ, qwq, qaq) for (i64 QWQ = (qwq); (QWQ) >= (qaq); QWQ--)
+
+#define dbg(x) std::cerr << (#x) << " : " << x << "\n";
+#define dbendl std::cerr << "\n"; 
+#define db std::cerr
+
+#define lookMem std::cerr << abs (&MemST - &MemED) / 1024.0 / 1024.0 << "MB defined\n";
+#define lookTime std::cerr << (double) (clock () - TimeST) / CLOCKS_PER_SEC << "s used\n";
+int TimeST;
+bool MemST;
+#define MultiTask lovely_fairytale
+
+void solve() {
+	i64 n, k;
+	std::cin >> n >> k;
+
+	if (k == 0) return std::cout << 1 << "\n", void(0);
+
+	i64 l = n / (k + 1);
+	i64 r = n / k;
+
+	std::cout << r - l << "\n";
+}
+
+bool MemED;
+int main() {
+	fastread
+	// lookMem	
+	// TimeST = clock ();
+
+#ifndef MultiTask
+	int _ = 1;
+#else
+	int _; std::cin >> _;
+#endif
+	
+	while (_--) {
+		solve();
+	}
+
+	return 0;
+}
