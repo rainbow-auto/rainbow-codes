8641e6e8f5830c6eb319fa1880534b8a2f034d0b
rainbow-auto <caotianlang@outlook.com>
2024-11-05 08:35:21 +0800

    P2949

---
+++ Luogu/P2949.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+// #pragma GCC optimize(2)
+
+using i64 = long long;
+using f64 = long double;
+
+#define fastread std::ios::sync_with_stdio (false); std::cin.tie(nullptr);
+
+#define rep(QWQ, qwq, qaq) for (i64 QWQ = (qwq); (QWQ) <= (qaq); QWQ++)
+#define per(QWQ, qwq, qaq) for (i64 QWQ = (qwq); (QWQ) >= (qaq); QWQ--)
+
+#define dbg(x) std::cerr << (#x) << " : " << x << "\n";
+#define dbendl std::cerr << "\n"; 
+#define db std::cerr
+
+#define lookMem std::cerr << abs (&MemST - &MemED) / 1024.0 / 1024.0 << "MB defined\n";
+#define lookTime std::cerr << (double) (clock () - TimeST) / CLOCKS_PER_SEC << "s used\n";
+int TimeST;
+bool MemST;
+// #define MultiTask lovely_fairytale
+
+const int maxn = 1000005;
+
+int n;
+
+struct Node {
+	int d, v;
+} a[maxn];
+
+void solve() {
+	std::cin >> n;
+
+	rep (i, 1, n) std::cin >> a[i].d >> a[i].v;
+
+	std::sort(a + 1, a + n + 1, [&](Node x, Node y) -> bool { return x.d < y.d; });
+
+	std::priority_queue<int> q;
+	i64 ans = 0;
+
+	rep (i, 1, n) {
+		if ((int) q.size() >= a[i].d) {
+			if ((-q.top()) < a[i].v) {
+				ans -= (-q.top()); q.pop();
+				q.push(-a[i].v);
+				ans += a[i].v;
+			}
+		} else {
+			q.push(-a[i].v);
+			ans += a[i].v; 
+		}
+	}
+
+	std::cout << ans << "\n";
+}
+
+bool MemED;
+int main() {
+	fastread
+	// lookMem	
+	// TimeST = clock ();
+
+#ifndef MultiTask
+	int _ = 1;
+#else
+	int _; std::cin >> _;
+#endif
+	
+	while (_--) {
+		solve();
+	}
+
+	return 0;
+}
