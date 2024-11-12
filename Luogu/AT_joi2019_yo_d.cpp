09b56104677501a223866602b28a31b5651a09ba
caotianlang <caotianlang@outlook.com>
2024-11-12 10:15:52 +0800

    AT_joi2019_yo_d

---
+++ Luogu/AT_joi2019_yo_d.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+// #pragma GCC optimize(2)
+
+using i64 = long long;
+using f64 = long double;
+
+#define fastread std::ios::sync_with_stdio (false); std::cin.tie(nullptr);
+
+#define rep(QWQ, qwq, qaq) for (i64 QWQ = (qwq); (QWQ) <= (qaq); QWQ++)
+#define per(QWQ, qwq, qaq) for (i64 QWQ = (qwq); (QWQ) >= (qaq); QWQ--)
+
+#define dbg(x) std::cerr << (#x) << " : " << x << "\n";
+#define dbendl std::cerr << "\n"; 
+#define db std::cerr
+
+#define lookMem std::cerr << abs (&MemST - &MemED) / 1024.0 / 1024.0 << "MB defined\n";
+#define lookTime std::cerr << (double) clock() / CLOCKS_PER_SEC << "s used\n";
+int TimeST;
+bool MemST;
+// #define MultiTask lovely_fairytale
+
+const int maxn = 100005;
+
+int n;
+int a[maxn];
+
+// 111111111
+// 010000110
+
+bool b[maxn];
+
+void solve() {
+	std::cin >> n;
+	rep (i, 1, n) std::cin >> a[i];
+
+	std::map<int, std::vector<int>> pos;
+	rep (i, 1, n) if (a[i] != 0) pos[-a[i]].push_back(i);
+
+	int mx = 0;
+	int cur = 0;
+	for (auto _ : pos) {
+		int x; std::vector<int> p; std::tie(x, p) = _;
+
+		for (auto i : p) {
+			b[i] = true;
+			int dt = 1;
+			if (b[i - 1]) dt--;
+			if (b[i + 1]) dt--;
+			cur += dt;
+		}
+		mx = std::max(mx, cur);
+	}
+
+	std::cout << mx << "\n";
+}
+
+bool MemED;
+int main() {
+	fastread
+	// lookMem	
+
+#ifndef MultiTask
+	int _ = 1;
+#else
+	int _; std::cin >> _;
+#endif
+	
+	while (_--) {
+		solve();
+	}
+
+	return 0;
+}
