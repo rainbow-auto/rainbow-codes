5b08e5adb5f2925f9e82af5e7cd3399d1ff03014
caotianlang <caotianlang@outlook.com>
2024-11-12 11:19:57 +0800

    P1803

---
+++ Luogu/P1803.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+// #pragma GCC optimize(2)
+
+using i64 = long long;
+using f64 = long double;
+
+#define fastread std::ios::sync_with_stdio (false); std::cin.tie(nullptr);
+
+#define rep(QWQ, qwq, qaq) for (i64 QWQ = (qwq); (QWQ) <= (qaq); QWQ++)
+#define per(QWQ, qwq, qaq) for (i64 QWQ = (qwq); (QWQ) >= (qaq); QWQ--)
+
+#define dbg(x) std::cerr << (#x) << " : " << x << "\n";
+#define dbendl std::cerr << "\n"; 
+#define db std::cerr
+
+#define lookMem std::cerr << abs (&MemST - &MemED) / 1024.0 / 1024.0 << "MB defined\n";
+#define lookTime std::cerr << (double) clock() / CLOCKS_PER_SEC << "s used\n";
+int TimeST;
+bool MemST;
+// #define MultiTask lovely_fairytale
+
+const int maxn = 1000005;
+
+int n;
+struct Range {
+	int l, r;
+	friend bool operator < (Range a, Range b) {
+		return a.r < b.r;
+	}
+} rgs[maxn];
+
+void solve() {
+	std::cin >> n;
+	rep (i, 1, n) std::cin >> rgs[i].l >> rgs[i].r;
+
+	std::sort(rgs + 1, rgs + n + 1);	
+
+	int lst = -1;
+	int ans = 0;
+	rep (i, 1, n) {
+		if (rgs[i].l >= lst) ans++, lst = rgs[i].r;
+	}
+
+	std::cout << ans << "\n";
+}
+
+bool MemED;
+int main() {
+	fastread
+	// lookMem	
+
+#ifndef MultiTask
+	int _ = 1;
+#else
+	int _; std::cin >> _;
+#endif
+	
+	while (_--) {
+		solve();
+	}
+
+	return 0;
+}
