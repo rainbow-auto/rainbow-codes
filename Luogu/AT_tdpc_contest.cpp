7ab8faf0e23b185d2fb57004b9b5f7e785175051
caotianlang <caotianlang@outlook.com>
2024-11-18 18:57:52 +0800

    AT_tdpc_contest

---
+++ Luogu/AT_tdpc_contest.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+// #pragma GCC optimize(2)
+
+using i64 = long long;
+using f64 = double;
+
+#define fastread std::ios::sync_with_stdio (false); std::cin.tie(nullptr);
+
+#define rep(QWQ, qwq, qaq) for (i64 QWQ = (qwq); (QWQ) <= (qaq); QWQ++)
+#define per(QWQ, qwq, qaq) for (i64 QWQ = (qwq); (QWQ) >= (qaq); QWQ--)
+
+#define dbg(x) std::cerr << (#x) << " : " << x << "\n";
+#define dbendl std::cerr << "\n"; 
+#define db std::cerr
+
+#define lookMem std::cerr << abs (&MemST - &MemED) / 1024.0 / 1024.0 << "MB defined\n";
+#define lookTime std::cerr << (double) clock() / CLOCKS_PER_SEC << "s used\n";
+int TimeST;
+bool MemST;
+// #define MultiTask lovely_fairytale
+
+const int maxn = 10005;
+
+int n;
+bool f[maxn];
+
+void solve() {
+	std::cin >> n;
+
+	f[0] = true;
+	rep (i, 1, n) {
+		int x; std::cin >> x;
+		per (i, 10000, x) f[i] |= f[i - x];
+	}
+
+	i64 ans = 0;
+	rep (i, 0, 10000) ans += f[i];
+	std::cout << ans << "\n";
+}
+
+bool MemED;
+int main() {
+	fastread
+	// lookMem	
+
+#ifndef MultiTask
+	int _ = 1;
+#else
+	int _; std::cin >> _;
+#endif
+	
+	while (_--) {
+		solve();
+	}
+
+	return 0;
+}
