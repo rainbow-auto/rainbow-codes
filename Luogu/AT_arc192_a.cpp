0060ecc152dfecb8b04500541565ec70403a5faa
rainbow-auto <caotianlang@outlook.com>
2025-03-22 20:20:02 +0800

    AT_arc100_a

--- Luogu/AT_arc192_a.cpp
+++ Luogu/AT_arc192_a.cpp
@@ -6,7 +6,7 @@ using f64 = double;
 
 #define fastread std::ios::sync_with_stdio (false); std::cin.tie(nullptr);
 
-#define rep(QWQ, qwq, qaq) for (i64 QWQ = (qwq); (QWQ) <= (qaq); QWQ++)
+#define rep(QWQ, qwq, qaq) for (int QWQ = (qwq); (QWQ) <= (qaq); QWQ++)
 #define per(QWQ, qwq, qaq) for (i64 QWQ = (qwq); (QWQ) >= (qaq); QWQ--)
 
 #define dbg(x) std::cerr << (#x) << " : " << x << "\n";
@@ -19,26 +19,43 @@ int TimeST;
 bool MemST;
 // #define MultiTask lovely_fairytale
 
-const int maxn = 200005;
+const int maxn = 2000005;
 
 int n;
 int a[maxn];
 
 void solve() {
 	std::cin >> n;
-	rep (i, 1, n) {
-		int x; std::cin >> x;
-		a[i] = x - i;
+	rep (i, 1, n) std::cin >> a[i];
+	rep (i, 1, n) a[n + i] = a[i];
+
+	bool flg = true;
+	rep (i, 1, n) flg &= a[i] == 1;
+	if (flg) return std::cout << "Yes\n", void(0);
+
+	if (n % 4 == 0) return std::cout << "Yes\n", void(0);
+	if (n % 4 == 1 or n % 4 == 3) {
+		int mx = 0;
+		int lst = 0x3f3f3f3f;
+		rep (i, 1, (n << 1)) {
+			if (a[i]) continue;
+			mx = std::max(mx, i - lst - 1);
+			lst = i;
+		}
+		if (mx < 1) return std::cout << "No\n", void(0);
+	}
+	if (n % 4 == 2) {
+		bool flg = false;
+		int lst = -1;
+		rep (i, 1, (n << 1)) {
+			if (not a[i]) continue;
+			if (~lst) flg |= ((i - lst - 1) & 1) == 0;
+			lst = i;
+		}
+		if (not flg) return std::cout << "No\n", void(0);
 	}
 
-	std::sort(a + 1, a + n + 1);
-
-	i64 b = a[((n + 1) >> 1)];
-
-	i64 ans = 0;
-	rep (i, 1, n) ans += std::abs(a[i] - b); 
-
-	std::cout << ans << "\n";
+	std::cout << "Yes\n";
 }
 
 bool MemED;
