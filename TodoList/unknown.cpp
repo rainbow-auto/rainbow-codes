364cf94b98ca8fb4d579ff7e836f9e50dc030dc0
曹天朗 <ctlqwq@ctlqwq-MacBook-Air.local>
2025-06-14 18:50:26 +0800

    unknown

---
+++ TodoList/unknown.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+// #pragma GCC optimize(2)
+
+using i64 = long long;
+using f64 = double;
+
+#define fastread std::ios::sync_with_stdio (false); std::cin.tie(nullptr);
+
+#define rep(QWQ, qwq, qaq) for (i64 QWQ = (qwq); (QWQ) <= (qaq); QWQ++)
+#define per(QWQ, qwq, qaq) for (i64 QWQ = (qwq); (QWQ) >= (qaq); QWQ--)
+
+#define dbg(x) std::cerr << (#x) << " : " << x << "\n";
+#define dbendl std::cerr << "\n"; 
+#define db std::cerr
+
+#define lookMem std::cerr << abs (&MemST - &MemED) / 1024.0 / 1024.0 << "MB defined\n";
+#define lookTime std::cerr << (double) clock() / CLOCKS_PER_SEC << "s used\n";
+int TimeST;
+bool MemST;
+// #define MultiTask lovely_fairytale
+
+const int maxn = 1000005;
+
+int n;
+bool a[maxn];
+
+void solve() {
+	std::string s; std::cin >> s;
+	int n = (int) s.size();
+	s = " " + s;
+
+	rep (i, 1, n) {
+		char ch = s[i];
+		a[i] = ch == 'B';
+	}	
+
+	int suf = 0;
+	i64 ans = 0;
+	per (i, n, 1) {
+		if (not a[i]) suf++;
+		if (a[i]) ans += suf;
+	}
+
+	std::cout << ans << "\n";
+}
+
+bool MemED;
+int main() {
+	fastread
+	// lookMem	
+
+#ifndef MultiTask
+	int _ = 1;
+#else
+	int _; std::cin >> _;
+#endif
+	
+	while (_--) {
+		solve();
+	}
+
+	return 0;
+}
