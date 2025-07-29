e35b7fc93dc95f9866c72d4c95ae4058879b6dac
曹天朗 <ctlqwq@ctlqwqs-MacBook-Air.local>
2025-07-29 16:24:27 +0800

    P2424

---
+++ Luogu/P2424.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+// #pragma GCC optimize(2)
+
+using i64 = long long;
+using f64 = double;
+
+#define fastread std::ios::sync_with_stdio (false); std::cin.tie(nullptr);
+
+#define rep(QWQ, qwq, qaq) for (i64 QWQ = (qwq); (QWQ) <= (qaq); QWQ++)
+#define per(QWQ, qwq, qaq) for (i64 QWQ = (qwq); (QWQ) >= (qaq); QWQ--)
+
+#define dbg(x) std::cerr << (#x) << " : " << x << "\n";
+#define dbendl std::cerr << "\n"; 
+#define db std::cerr
+
+#define lookMem std::cerr << abs (&MemST - &MemED) / 1024.0 / 1024.0 << "MB defined\n";
+#define lookTime std::cerr << (double) clock() / CLOCKS_PER_SEC << "s used\n";
+int TimeST;
+bool MemST;
+// #define MultiTask lovely_fairytale
+#define file(x) std::freopen(x".in", "r", stdin); std::freopen(x".out", "w", stdout);
+
+inline i64 qry(i64 n) {
+	if (n <= 1) return n;
+	i64 res = 0;
+	for (i64 l = 1, r; l <= n; l = r + 1) {
+		r = n / (n / l);
+		res += (n / l) * (r - l + 1) * (r + l) / 2;
+	}
+	return res;
+}
+
+void solve() {
+	int l, r; std::cin >> l >> r;
+	std::cout << qry(r) - qry(l - 1) << "\n";
+}
+
+bool MemED;
+int main() {
+	fastread
+	// lookMem	
+
+#ifndef MultiTask
+	int _ = 1;
+#else
+	int _; std::cin >> _;
+#endif
+	
+	while (_--) {
+		solve();
+	}
+
+	return 0;
+}
