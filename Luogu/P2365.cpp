687dbf36ab604ee984b9853331a8c906521d19eb
曹天朗 <ctlqwq@ctlqwqs-MacBook-Air.local>
2025-08-17 19:35:53 +0800

    P2365

---
+++ Luogu/P2365.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+// #pragma GCC optimize(2)
+
+using i64 = long long;
+using f64 = double;
+
+#define fastread std::ios::sync_with_stdio (false); std::cin.tie(nullptr);
+
+#define rep(QWQ, qwq, qaq) for (i64 QWQ = (qwq); (QWQ) <= (qaq); QWQ++)
+#define per(QWQ, qwq, qaq) for (i64 QWQ = (qwq); (QWQ) >= (qaq); QWQ--)
+
+#define dbg(x) std::cerr << (#x) << " : " << x << "\n";
+#define dbendl std::cerr << "\n"; 
+#define db std::cerr
+
+#define lookMem std::cerr << abs (&MemST - &MemED) / 1024.0 / 1024.0 << "MB defined\n";
+#define lookTime std::cerr << (double) clock() / CLOCKS_PER_SEC << "s used\n";
+int TimeST;
+bool MemST;
+// #define MultiTask lovely_fairytale
+#define file(x) std::freopen(x".in", "r", stdin); std::freopen(x".out", "w", stdout);
+
+constexpr int maxn = 5005;
+
+int n;
+
+i64 s;
+
+i64 t_sum[maxn], f_sum[maxn];
+i64 f[maxn];
+
+void solve() {
+	std::cin >> n;
+	std::cin >> s;
+	rep (i, 1, n) std::cin >> t_sum[i] >> f_sum[i];
+	
+	rep (i, 1, n) t_sum[i] += t_sum[i - 1];
+	rep (i, 1, n) f_sum[i] += f_sum[i - 1];
+
+	std::memset(f, 0x3f, sizeof f);
+	f[0] = 0;
+	rep (i, 1, n) {
+		rep (j, 0, i - 1) {
+			f[i] = std::min(f[i], f[j] + (f_sum[i] - f_sum[j]) * t_sum[i] + (f_sum[n] - f_sum[j]) * s);
+		}
+	}
+
+	std::cout << f[n] << "\n";
+}
+
+bool MemED;
+int main() {
+	fastread
+	// lookMem	
+
+#ifndef MultiTask
+	int _ = 1;
+#else
+	int _; std::cin >> _;
+#endif
+	
+	while (_--) {
+		solve();
+	}
+
+	return 0;
+}
