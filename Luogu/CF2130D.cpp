1970b6e9a156635725c5daa3549161b3561cc4cb
曹天朗 <ctlqwq@ctlqwqs-MacBook-Air.local>
2025-09-20 18:50:30 +0800

    CF2130D

---
+++ Luogu/CF2130D.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+// #pragma GCC optimize(2)
+
+using i64 = long long;
+using f64 = double;
+
+#define fastread std::ios::sync_with_stdio (false); std::cin.tie(nullptr);
+
+#define rep(QWQ, qwq, qaq) for (i64 QWQ = (qwq); (QWQ) <= (qaq); QWQ++)
+#define per(QWQ, qwq, qaq) for (i64 QWQ = (qwq); (QWQ) >= (qaq); QWQ--)
+
+#define dbg(x) std::cerr << (#x) << " : " << x << "\n";
+#define dbendl std::cerr << "\n"; 
+#define db std::cerr
+
+#define lookMem std::cerr << abs (&MemST - &MemED) / 1024.0 / 1024.0 << "MB defined\n";
+#define lookTime std::cerr << (double) clock() / CLOCKS_PER_SEC << "s used\n";
+int TimeST;
+bool MemST;
+#define MultiTask lovely_fairytale
+#define file(x) std::freopen(x".in", "r", stdin); std::freopen(x".out", "w", stdout);
+
+constexpr int maxn = 300005;
+
+int n;
+int a[maxn];
+
+void solve() {
+	std::cin >> n;
+	rep (i, 1, n) std::cin >> a[i];
+
+	i64 ans = 0;
+	rep (i, 1, n) {
+		int L = 0, R = 0;
+		rep (j, 1, i - 1) L += (a[j] > a[i]);
+		rep (j, i + 1, n) R += (a[j] > a[i]);
+		ans += std::min(L, R);
+	}
+
+	std::cout << ans << "\n";
+}
+
+bool MemED;
+int main() {
+	fastread
+	// lookMem	
+
+#ifndef MultiTask
+	int _ = 1;
+#else
+	int _; std::cin >> _;
+#endif
+	
+	while (_--) {
+		solve();
+	}
+
+	return 0;
+}
