7c9e726bfe404dc2907f5a3c79253c1040a6878d
曹天朗 <ctlqwq@ctlqwqs-MacBook-Air.local>
2025-10-07 21:01:32 +0800

    P4391

---
+++ Luogu/P4391.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+// #pragma GCC optimize(2)
+
+using i64 = long long;
+using f64 = double;
+
+#define fastread std::ios::sync_with_stdio (false); std::cin.tie(nullptr);
+
+#define rep(QWQ, qwq, qaq) for (i64 QWQ = (qwq); (QWQ) <= (qaq); QWQ++)
+#define per(QWQ, qwq, qaq) for (i64 QWQ = (qwq); (QWQ) >= (qaq); QWQ--)
+
+#define dbg(x) std::cerr << (#x) << " : " << x << "\n";
+#define dbendl std::cerr << "\n"; 
+#define db std::cerr
+
+#define lookMem std::cerr << abs (&MemST - &MemED) / 1024.0 / 1024.0 << "MB defined\n";
+#define lookTime std::cerr << (double) clock() / CLOCKS_PER_SEC << "s used\n";
+int TimeST;
+bool MemST;
+// #define MultiTask lovely_fairytale
+#define file(x) std::freopen(x".in", "r", stdin); std::freopen(x".out", "w", stdout);
+
+constexpr int maxn = 1000005;
+
+int n;
+std::string s;
+
+int pi[maxn];
+
+void solve() {
+	std::cin >> n;
+	std::cin >> s;
+	s = " " + s;
+
+	pi[1] = 0;
+
+	rep (i, 2, n) {
+		int j = pi[i - 1];
+		while (j and s[j + 1] != s[i]) j = pi[j];
+		if (s[j + 1] == s[i]) j++;
+		pi[i] = j;
+	}
+
+	std::cout << n - pi[n] << "\n";
+}	
+
+bool MemED;
+int main() {
+	fastread
+	// lookMem	
+
+#ifndef MultiTask
+	int _ = 1;
+#else
+	int _; std::cin >> _;
+#endif
+	
+	while (_--) {
+		solve();
+	}
+
+	return 0;
+}
